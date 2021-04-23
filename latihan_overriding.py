811a13cfe13bc088e049ceab1b58e83b3b64386b
IqbalHadiSubekti <33026916+IqbalHadiSubekti@users.noreply.github.com>
2021-04-23 22:23:03 +0700

    yes
    yes

---
+++ latihan_overriding.py
@@ -0,0 +1,45 @@
+from math import pi
+
+class bangunRuang():
+
+	def __init__(self):
+		super(bangunRuang,self).__init__()
+		self.nama = "Bangun Ruang"
+		self.luasa = None
+		self.tinggi = None
+
+	def volume(self,luasa,tinggi):
+		self.volume = self.luasa*self.tinggi
+		return self.volume
+
+class tabung(bangunRuang):
+	def __init__(self,tinggi,jari):
+		super().__init__()
+		self.tinggi = float(input("Masukkan tinggi: "))
+		self.jari = float(input("Masukkan jari: "))
+
+	def volumeTabung(self):
+		self.volumeTabung = pi*self.jari*self.jari*self.tinggi
+		return self.volumeTabung
+
+class balok(bangunRuang):
+	def __init__(self,tinggi,panjang,lebar):
+		super().__init__()
+		self.panjang = float(input("Masukkan panjang: "))
+		self.lebar = float(input("Masukkan lebar: "))
+		self.tinggi = float(input("Masukkan tinggi: "))
+
+	def volumeBalok(self):
+		self.volumeBalok = self.panjang*self.lebar*self.tinggi
+		return self.volumeBalok
+
+print("TABUNG")
+nilai = tabung("","")
+print("Volume Tabung yaitu:",nilai.volumeTabung())
+
+
+print()
+
+print("BALOK")
+hitung = balok("","","")
+print("Volume balok yaitu:",hitung.volumeBalok())
